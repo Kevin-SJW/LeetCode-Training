4b995f653bcefde92796b8e6f10c1e9794ee44c5
Kevin <shijianweicisco@163.com>
2019-10-16 16:29:46 +0800

    LengthOfLongestSubstring without repeating character

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -19,7 +19,7 @@ public class LengthOfLongestSubstring {
         int ans=0,i=0,j=0;
         int n=s.length();
         Set<Character> set=new HashSet<>();
-        while (i<n||j<n){
+        while (i<n&&j<n){
             if(!set.contains(s.charAt(j))){
                 set.add(s.charAt(j++));
             }else{
