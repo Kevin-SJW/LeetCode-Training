f73c9a5362b322273956ad32bae7d82fdd645f93
Kevin <shijianweicisco@163.com>
2019-10-24 14:57:37 +0800

    three Sum

--- ThreeSum.java
+++ ThreeSum.java
@@ -14,38 +14,39 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> res=threeSum(nums);
+        List<List<Integer>> res = threeSum(nums);
         System.out.println(res);
 
     }
+
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> lists=new ArrayList<>();
+        List<List<Integer>> lists = new ArrayList<>();
         Arrays.sort(nums);
 
-        for(int k=0;k<nums.length-2;k++){
-            int i=k+1;
-            int j=nums.length-1;
-            int target=0-nums[k];
-            while(i<j){
-                if(target==nums[i]+nums[j]){
-                    lists.add(Arrays.asList(nums[k],nums[i++],nums[j--]));
-                    while(i<j&&nums[i]==nums[i-1]){
+        for (int k = 0; k < nums.length - 2; k++) {
+            int i = k + 1;
+            int j = nums.length - 1;
+            int target = 0 - nums[k];
+            while (i < j) {
+                if (target == nums[i] + nums[j]) {
+                    lists.add(Arrays.asList(nums[k], nums[i++], nums[j--]));
+                    while (i < j && nums[i] == nums[i - 1]) {
                         i++;
                     }
-                    while(i<j&&nums[j]==nums[j+1]){
+                    while (i < j && nums[j] == nums[j + 1]) {
                         j--;
                     }
 
-                }else if(nums[k]+nums[i]+nums[j]<target){
+                } else if (nums[k] + nums[i] + nums[j] < target) {
                     i++;
-                }else{
+                } else {
                     j--;
                 }
             }
             //防止重复
-                while(k==nums.length-2||nums[k]==nums[k+1]){
-                    k++;
-                }
+            while (k <nums.length - 2 && nums[k] == nums[k + 1]) {
+                k++;
+            }
         }
         return lists;
     }
