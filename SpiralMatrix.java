735248a515fb3817a0d7f6295e907630bcd43f13
Kevin <shijianweicisco@163.com>
2019-12-16 16:12:53 +0800

    SpiralMatrix

---
+++ SpiralMatrix.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Classname SpiralMatrix
+ * @Description TODO
+ * @Date 2019/12/16 15:27
+ * @Created by 14241
+ */
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printMatrix(matrix);
+        System.out.println();
+        System.out.println(spiralOrder(matrix));
+
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        if (matrix.length == 0) {
+            return ans;
+        }
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) {
+                ans.add(matrix[r1][c]);
+            }
+            for (int r = r1 + 1; r <= r2; r++) {
+                ans.add(matrix[r][c2]);
+            }
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) {
+                    ans.add(matrix[r2][c]);
+                }
+                for (int r = r2; r > r1; r--) {
+                    ans.add(matrix[r][c1]);
+                }
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
