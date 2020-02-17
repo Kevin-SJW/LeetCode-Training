3c19dca1dec53fffa673594b930e8987867e00b6
Kevin <shijianweicisco@163.com>
2020-02-17 15:15:38 +0800

    Path Sum 2

--- PathSum2.java
+++ PathSum2.java
@@ -25,8 +25,8 @@ public class PathSum2 {
         int sum=30;
         List<List<Integer>> res=pathSum(root,sum);
         for(List<Integer> nums:res){
-            for(int num:nums){
-                System.out.print(num+" ");
+            for(int nu:nums){
+                System.out.print(nu+" ");
             }
         }
 
