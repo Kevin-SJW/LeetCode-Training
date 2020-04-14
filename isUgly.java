d40009642f854c5f8fc66b8aab12dd622f9f7c33
Kevin <shijianweicisco@163.com>
2020-04-14 15:46:21 +0800

    isUgly.java

---
+++ isUgly.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * @Classname isUgly
+ * @Description TODO
+ * @Date 2020/4/14 15:36
+ * @Created by 14241
+ */
+public class isUgly {
+    public static void main(String[] args) {
+        int num=6;
+        System.out.println(isUgly(num));
+    }
+
+    public static boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            num = num / 2;
+            return isUgly(num);
+        }
+        if (num % 3 == 0) {
+            num = num / 3;
+            return isUgly(num);
+        }
+        if (num % 5 == 0) {
+            num = num / 5;
+            return isUgly(num);
+        }
+        return false;
+    }
+}
