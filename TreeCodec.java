d07b903933c13de1ad8b7490d32668c00b1bea28
Kevin <shijianweicisco@163.com>
2020-04-20 16:44:06 +0800

    TreeCodec.java

---
+++ TreeCodec.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Classname TreeCodec
+ * @Description TODO
+ * @Date 2020/4/20 16:43
+ * @Created by 14241
+ */
+public class TreeCodec {
+    //Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode curt = queue.poll();
+            if (curt == null){
+                sb.append("null,");
+                continue;
+            }
+            sb.append(curt.val);
+            sb.append(",");
+            queue.offer(curt.left);
+            queue.offer(curt.right);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    //"[1,2,3,null,null,4,5,null,null]"
+    public TreeNode deserialize(String data) {
+        if (data == null || data.equals("")){
+            return null;
+        }
+        String[] nodes = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        for (int i = 1; i < nodes.length; i++){
+            TreeNode curt = queue.poll();
+            if (!nodes[i].equals("null")){
+                curt.left = new TreeNode(Integer.parseInt(nodes[i]));
+                queue.offer(curt.left);
+            }
+            i += 1;
+            if (!nodes[i].equals("null")){
+                curt.right = new TreeNode(Integer.parseInt(nodes[i]));
+                queue.offer(curt.right);
+            }
+        }
+        return root;
+    }
+}
