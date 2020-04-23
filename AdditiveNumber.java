df5d75b0dfe20c04b026c6150da5ecac12551473
Kevin <shijianweicisco@163.com>
2020-04-23 16:05:46 +0800

    AdditiveNumber.java

---
+++ AdditiveNumber.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * @Classname AdditiveNumber
+ * @Description TODO
+ * @Date 2020/4/23 15:52
+ * @Created by 14241
+ */
+public class AdditiveNumber {
+    public static void main(String[] args) {
+        String num = "112358";
+        System.out.println(isAdditiveNumber(num));
+
+    }
+
+    public static boolean isAdditiveNumber(String num) {
+        int len = num.length();
+        for (int i = 1; i < len / 2; i++) {
+            for (int j = 1; Math.max(i, j) <= len - i - j; j++) {
+                if (isValid(i, j, num)) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean isValid(int i, int j, String num) {
+        if (num.charAt(0) == '0' && j > 1) {
+            return false;
+        }
+        if (num.charAt(i) == '0' && j > 1) {
+            return false;
+        }
+        String sum;
+        Long one = Long.parseLong((num.substring(0, i)));
+        Long two = Long.parseLong(num.substring(i, i + j));
+        for (int s = i + j; s < num.length(); s += num.length()) {
+            two = two + one;
+            one = two - one;
+            sum = two.toString();
+            if (!num.startsWith(sum, s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
